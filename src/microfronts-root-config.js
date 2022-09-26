d56f127965eee79399aa083db36da4bfb855d1b4
Lion <breakermoob@hotmail.com>
2022-09-26 17:03:05 -0500

    feat: initial commit & basic config with layout engine

---
+++ src/microfronts-root-config.js
@@ -0,0 +1,27 @@
+import { registerApplication, start } from "single-spa";
+import {
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
+} from "single-spa-layout";
+import microfrontendLayout from "./microfrontend-layout.html";
+
+const routes = constructRoutes(microfrontendLayout, {
+  loaders: {
+    microfrontLoader: "<h1>Loading</h1>",
+  },
+  errors: {
+    microfrontError: "<h1>Failed to load</h1>",
+  },
+});
+const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    return System.import(name);
+  },
+});
+const layoutEngine = constructLayoutEngine({ routes, applications });
+
+applications.forEach(registerApplication);
+layoutEngine.activate();
+start();
